a4cb335b68b7eeb1a2dd5d6bd93edd42edd99e45
Kathy Seol <kseol@merkleinc.com>
2024-09-19 16:51:00 -0400

    made background color change on body

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,6 +6,7 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
+    console.log("cool round");
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
